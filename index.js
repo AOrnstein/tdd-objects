e82d608d0c48129619bedf872efbe8aa7a8645b9
AOrnstein <ornstein.adam@gmail.com>
2026-04-29 10:09:00 -0600

    implement zip logic

--- index.js
+++ index.js
@@ -184,7 +184,11 @@ export function getTotalCost(cart) {
  * zip(["x"], ["x"]); // {x: "x"}
  */
 export function zip(keys, values) {
-  // TODO
+  let result = {};
+  for (let i = 0; i < keys.length; i++) {
+    result[keys[i]] = values[i];
+  }
+  return result;
 }
 
 /**
